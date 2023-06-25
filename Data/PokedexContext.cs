dcc8152b24256b472d3c883a3fbeced8363621ba
jandrzewiecki <jan.drzewiecki@jit.team>
2023-06-25 09:55:51 +0200

    adding trainer entity

--- Data/PokedexContext.cs
+++ Data/PokedexContext.cs
@@ -1,8 +1,10 @@
 using Microsoft.EntityFrameworkCore;
+using Pokedex.models;
 
 namespace Pokedex.Data;
 
 public class PokedexContext : DbContext
 {
     public PokedexContext(DbContextOptions<PokedexContext> options) : base(options) { }
+    public DbSet<Trainer> Trainers { get; set; }
 }
---
+++ models/Trainer.cs
@@ -0,0 +1,10 @@
+namespace Pokedex.models;
+
+public class Trainer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    
+    
+}
